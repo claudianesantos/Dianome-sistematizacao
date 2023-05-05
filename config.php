11fcb7b8b9cb05a7e2efafc6256028baf1724863
rayonny <rayonnygomes18@gmail.com>
2023-05-05 00:51:37 -0300

    formularioBD

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPasswolrd = '';
+$sbName = 'formulario-rayonny';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPasswolrd,$sbName);
+
+?>
